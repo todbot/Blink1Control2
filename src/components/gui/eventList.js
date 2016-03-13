eded1347b06a071dc24fea3643d099b7c1485ad4
Tod E. Kurt <tod@todbot.com>
2016-03-12 21:59:17 -0800

    fixed eventlist

--- src/components/gui/eventList.js
+++ src/components/gui/eventList.js
@@ -6,6 +6,8 @@ var Button = require('react-bootstrap').Button;
 var ListGroup = require('react-bootstrap').ListGroup;
 var ListGroupItem = require('react-bootstrap').ListGroupItem;
 
+var _ = require('lodash');
+
 var log = require('../../logger');
 
 var moment = require('moment');
@@ -24,7 +26,6 @@ var EventList = React.createClass({
 		this.getUpdates(); // set up polling, ugh FIXME:
 	},
 	getUpdates: function() {
-		// var rules = util.cheesyClone(IftttService.getRules());
 		var events = log.getLastEvents();
 		// log.msg('EventList.getUpdates',events);
 		this.setState({events:events});
@@ -37,19 +38,25 @@ var EventList = React.createClass({
 
 
 	render: function() {
+		var revevents = this.state.events.concat().reverse();
+
 		var createEventLine = function(event,index) {
 			//return (<li key={event.date}> {event.date} - {event.text} </li>);
 			var humantime = moment(event.date).format('dd LT');
+			var type = event.type;
+			var text = event.text;
+			var id = event.id;
+			var msg = <span><b>{type}</b> {text} - {id}</span>;
 			return (
-				<ListGroupItem key={index} style={{lineHeight:"100%", fontSize: "0.75em", textIndent:-10}}>{humantime} - {event.text} </ListGroupItem>);
+				<ListGroupItem key={index} style={{lineHeight:"100%", fontSize: "0.85em", textIndent:-10}}>{humantime}: {msg} </ListGroupItem>);
 		};
 
 		return (
 			<Panel header={<h4>Recent Events</h4>} style={{ width: 280, height: 300, margin:5, padding:0}}>
 				<ListGroup style={{	height: 200, overflowY: 'scroll', overflowX:'hidden', padding:0, margin:0, marginBottom:10}}>
-				{this.state.events.map(createEventLine, this)}
+				{revevents.map(createEventLine, this)}
 				</ListGroup>
-				<Button block bsSize="xsmall"  onClick={this.clearEvents}>Dismiss all</Button>
+				<Button block bsSize="xsmall"  onClick={this.clearEvents}>Clear events</Button>
 			</Panel>
 			);
 	}
--- src/logger.js
+++ src/logger.js
@@ -14,20 +14,22 @@ else {
 	isDevel = (process.env.NODE_ENV === 'development');
 }
 
+var eventsMax = 100;  // FIXME: put in configuration?
+
 // TO DO: add log to file
 // TO DO: add log levels
 
-// console.log("logger: isDevel",isDevel);
-// var events = [];
 var events = [
-	{ date: Date.now()-400000000, text: "this other thing happened"},
-	{ date: Date.now()-40000000, text: "this other thing happened"},
-	{ date: Date.now()-4000000, text: "this other thing happened"},
-	{ date: Date.now()-400000, text: "this 3.1 happened"},
-	{ date: Date.now()-200000, text: "this 3  happened"},
-	{ date: Date.now()-1, text: "this 2 happened"},
-	{ date: Date.now(), text: "this happened"}
 ];
+// [
+// 	{ date: Date.now()-800000000, text: "this other thing happened"},
+// 	{ date: Date.now()-800000000, text: "this other thing happened"},
+// 	{ date: Date.now()-700000000, text: "this other thing happened"},
+// 	{ date: Date.now()-600000000, text: "this 3.1 happened", type:'ifttt'},
+// 	{ date: Date.now()-500000000, text: "this 3  happened", type:'script'},
+// 	{ date: Date.now()-300000000, text: "this 2 happened"},
+// 	{ date: Date.now()-200000000, text: "this happened"}
+// ];
 
 var Logger = {
 
@@ -54,20 +56,44 @@ var Logger = {
 		console.error.apply(console, args);
 		// FIXME: log to file?
 	},
-	addEvent: function(event) { // string or {date,text} object
+	addEvent: function(event) { // string or {date,text,type,id} object
 		if( typeof event === 'string' ) {
-			event = {date: Date.now(), text: event};
+			event = {text: event};
 		}
 		if( !event.date ) { event.date = Date.now(); }
-		events.unshift(event);
+		if( !event.type ) { event.type = ''; }
+		if( !event.id ) { event.id = ''; }
+		events.push(event);
+		events.sort( function(a,b) {
+			if( a.date > b.date ) { return 1; }
+			if( a.date , b.date ) { return -1; }
+			return 0;
+		});
+
+		if( events.length > eventsMax ) {
+			this.msg("Log: dropped at eventMax");
+			events.unshift();
+		}
+	},
+	getEvents: function(spec) {
+		var typedEvents = events.filter( function(e) {
+			if( !spec ) { return true; }
+			return e.type === spec.type;
+		});
+		return typedEvents;
 	},
 	getLastEvents: function(n) {
 		n = n ? 10 : n;
 		n = n > events.length ? events.length : n;
 		var lastEvents = events.slice( 0, n);
-		// this.msg("Log.getLastEvents",lastEvents);
 		return lastEvents;
 	}
+	// this.msg("Log.getLastEvents",lastEvents);
+	// lastEvents = lastEvents.map( function(e) {
+	// 	var newe = {text: e.text, date:e.date, type: e.type, id:e.id};
+	// 	// newe.text = (e.type) ? e.type + ' - ' + e.text : e.text;
+	// 	return newe;
+	// });
 };
 
 module.exports = Logger;
