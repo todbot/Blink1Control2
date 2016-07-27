706bf3813452bd8c26cfbb569945aa1a0ed66a72
Tod E. Kurt <tod@todbot.com>
2016-07-27 15:37:25 -0700

    added event log window

--- app/components/gui/eventList.js
+++ app/components/gui/eventList.js
@@ -2,17 +2,16 @@
 
 var React = require('react');
 var Panel = require('react-bootstrap').Panel;
-var Button = require('react-bootstrap').Button;
-var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
-// var ButtonGroup = require('react-bootstrap').ButtonGroup;
 var ListGroup = require('react-bootstrap').ListGroup;
 var ListGroupItem = require('react-bootstrap').ListGroupItem;
 
-// var _ = require('lodash');
+var BrowserWindow = require('electron').remote.BrowserWindow;
 
 var log = require('../../logger');
 
 var moment = require('moment');
+
+var logWindow;
 
 var EventList = React.createClass({
 	propTypes: {
@@ -36,10 +35,50 @@ var EventList = React.createClass({
 	},
 
 	clearEvents: function() {
-		log.clearEvents();
+		log.clearEvents(); // FIXME: only clear display, not log?
 		this.setState({events: []});
 	},
 
+	// event is format:
+	// event = {
+	//  date: Date, // time of event as JS Date, if omitted == now
+	//  type: 'trigger', 'triggerOff', 'error', 'info'
+	//  source: 'ifttt, 'mail', etc. ==  event source 'type'
+	//  id: 'red demo'  // event source 'name'
+	//  text: 'blah blah'  // text of event
+	// }
+	showEventLog: function() {
+		var events = log.getEvents();
+		var info = '';
+		if( events.length === 0 ) {
+			info = 'no events to display';
+		} else {
+			info += '<style> table { width:100% } table, th, td { font-size:85%; font-family:sans-serif; padding:5px; border:1px solid grey; border-collapse:collapse; }</style>';
+			info += '<table>';
+			info += '<tr><th>date</th><th>type</th><th>source</th><th>id</th><th>text</th></tr>';
+			events.map( function(e) {
+				var humantime = moment(e.date).format('LTS');
+				info += '<tr><td>' + humantime + '</td><td>' + e.type +
+				'</td><td>' + e.source + '</td><td>' + e.id + '</td><td>'+ e.text+'</td></tr>';
+			});
+			info += '</table>';
+		}
+		if( logWindow ) {
+			logWindow.show();
+		} else {
+			logWindow = new BrowserWindow({
+					alwaysOnTop: true,
+					autoHideMenuBar: true,
+					height: 300,
+					width: 400
+					// icon: assets['icon-32'],
+				});
+			logWindow.on("closed", function() {
+				logWindow = null;
+			});
+		}
+		logWindow.loadURL( 'data:text/html,' + info);
+	},
 	render: function() {
 		var revevents = this.state.events.concat().reverse();
 		var createEventLine = function(event,index) {
@@ -52,9 +91,11 @@ var EventList = React.createClass({
 				<ListGroupItem key={index} style={{lineHeight:"100%", fontSize: "0.85em", textIndent:-10}}><i style={{fontSize:'90%'}}>{humantime}:</i> {msg} </ListGroupItem>
 			);
 		};
-		var header = <h4>Recent Events <button style={{float:'right',fontSize:'80%' }}
-			bsStyle='link' onClick={this.clearEvents}>Clear</button></h4> ;
-
+		var header =
+			<h4>Recent Events
+				<button style={{float:'right',fontSize:'80%' }} bsStyle='link' onClick={this.showEventLog}>Log</button>
+				<button style={{float:'right',fontSize:'80%' }} bsStyle='link' onClick={this.clearEvents}>Clear</button>
+			</h4> ;
 		return (
 			<Panel header={header} style={{ width: 280, height: 305, margin:5, padding:0}}>
 				<ListGroup style={{	height: 240, overflowY: 'scroll', overflowX:'hidden', padding:0, margin:0, marginBottom:10}}>
