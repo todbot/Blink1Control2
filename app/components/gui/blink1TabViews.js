9750713e641f79bf133616c766db30923ba11021
Tod E. Kurt <tod@todbot.com>
2016-07-27 16:39:39 -0700

    open preferences via main menu

--- app/components/gui/blink1TabViews.js
+++ app/components/gui/blink1TabViews.js
@@ -1,26 +1,30 @@
 "use strict";
 
 var React = require('react');
-// var TabbedArea = require('react-bootstrap').TabbedArea;
-// var TabPane = require('react-bootstrap').TabPane;
+
 var Tabs = require('react-bootstrap').Tabs;
 var Tab = require('react-bootstrap').Tab;
-// var Panel = require('react-bootstrap').Panel;
-// var Well = require('react-bootstrap').Well;
+var Button = require('react-bootstrap').Button;
 
 var BigButtonSet = require('./bigButtonSet');
-// var IftttTable = require('./iftttTable');
-// var MailTable = require('./mailTable');
 var ToolTable = require('./toolTable');
+
+// var BrowserWindow = require('electron').remote.BrowserWindow;
+var ipcRenderer = require('electron').ipcRenderer;
+
 
 
 var Blink1TabViews = React.createClass({
 
-	render: function() {
+	openHelpWindow: function() {
+		ipcRenderer.send('openHelpWindow');
+	},
 
+	render: function() {
 		return (
 				<div style={{width:705}}>
-					<Tabs defaultActiveKey={1} animation={false} id='blink1tabview'>
+					<div style={{float:'right'}}><Button bsStyle="link" onClick={this.openHelpWindow}>Help</Button></div>
+					<Tabs defaultActiveKey={1} animation={false} id='blink1tabview'  >
 						<Tab eventKey={1} title={<i className="fa fa-long-arrow-right"> <b>Buttons</b></i>}>
 							<div style={{height: 215, padding: 5, margin: 0, background: "#fff", border: "solid 1px #ddd"}}>
 								<BigButtonSet />
@@ -29,11 +33,6 @@ var Blink1TabViews = React.createClass({
 						<Tab eventKey={2} title={<i className="fa fa-plug"> Event Sources</i>}>
 							<div style={{height: 215, padding: 5, margin: 0, background: "#fff", border: "solid 1px #ddd"}}>
 								<ToolTable />
-							</div>
-						</Tab>
-						<Tab eventKey={3} title={<i className="fa fa-life-ring"> Help</i>}>
-							<div style={{height: 215, padding: 5, margin: 0, background: "#fff", border: "solid 1px #ddd"}}>
-								Help goes here
 							</div>
 						</Tab>
 					</Tabs>
