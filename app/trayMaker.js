e92567f85c39211029b5aeb8593eae26d8798314
Tod E. Kurt <tod@todbot.com>
2016-08-20 13:22:13 -0700

    trayMaker now ready to realtime modify tray menus

--- app/trayMaker.js
+++ app/trayMaker.js
@@ -39,82 +39,7 @@ var pressBigButton = function( event,arg ) {
     }
 };
 
-var bigButtonsConfig = config.readSettings('bigButtons') || [];
-var statusButtons = bigButtonsConfig.map( function(bb,idx) {
-    return {
-        label: bb.name,
-        // icon: swatchIcon,
-        click: function(/*item*/) {
-            // console.log("click item",item);
-            pressBigButton(null,idx);
-            // mainWindow.webContents.send('pressBigButton', idx);
-        }
-    };
-});
 
-var contextMenuTemplate = [
-    { label: 'Blink1Control2 is running', enabled: false
-    },
-    // {	label: 'About ' + pkg.productName,
-    // 	click: function() { openAboutWindow(); }
-    // },
-    // {	type: "separator" },
-    {	label: 'Start minimized',
-        type: 'checkbox',
-        checked: config.readSettings('startup:startMinimized'),
-        click: function(menuItem) {
-            config.saveSettings('startup:startMinimized', menuItem.checked);
-        },
-    },
-    {	label: 'Start at login',
-        type: 'checkbox',
-        checked: config.readSettings('startup:startAtLogin'),
-        click: function(menuItem) {
-            config.saveSettings('startup:startAtLogin', menuItem.checked);
-            // test on Mac with:  osascript -e 'tell application "System Events" to get the name of every login item'
-            if( menuItem.checked ) {
-                blink1ControlAutoLauncher.enable();
-            } else {
-                blink1ControlAutoLauncher.disable();
-            }
-        }
-    },
-    {	label: 'Enable API server',
-        type: 'checkbox',
-        checked: config.readSettings('apiServer:enabled'),
-        click: function(menuItem) {
-            config.saveSettings('apiServer:enabled', menuItem.checked);
-            // mainWindow.webContents.send('reloadConfig', 'apiServer');
-        }
-    },
-    {	type: "separator" },
-    { 	label: 'Set Status...',
-        submenu: statusButtons
-    },
-    {	type: "separator" },
-    {	label: 'Open Settings...',
-        click: function() {
-            console.log("Open Settings...");
-            mainWindow.show();
-        }
-    },
-    {	label: 'Off / Reset Alerts',  // Note: only works when DevTools is hiddden, else Cmd-r reloads
-        accelerator: 'CommandOrControl+R',	// accelerator: 'CmdOrCtrl+R',
-        click: function() {
-            // mainWindow.webContents.send('resetAlerts', 'woot');
-            PatternsService.stopAllPatterns();
-            Blink1Service.off();
-        }
-    },
-    {	label: 'Quit',
-        // accelerator: 'CommandOrControl+Q',
-        // selector: 'terminate:',
-        click: function() {
-            // app.quit(); // doesn't actually cause app to quit
-    		ipcRenderer.send('quitnow');
-        }
-    }
-];
 // var devMenuTemplate = [
 // 	{	type: "separator" },
 // 	// {	label: 'Reload',
@@ -130,17 +55,98 @@ var contextMenuTemplate = [
 // ];
 
 var trayMaker = {
-    
+
     updateTrayMenu: function() {
+        console.log("TRAY MENU UPDATE...");
+        var bigButtonsConfig = config.readSettings('bigButtons') || [];
+        var statusButtons = bigButtonsConfig.map( function(bb,idx) {
+            return {
+                label: bb.name,
+                // icon: swatchIcon,
+                click: function(/*item*/) {
+                    // console.log("click item",item);
+                    pressBigButton(null,idx);
+                    // mainWindow.webContents.send('pressBigButton', idx);
+                }
+            };
+        });
+
+        var contextMenuTemplate = [
+            { label: 'Blink1Control2 is running', enabled: false
+            },
+            // {	label: 'About ' + pkg.productName,
+            // 	click: function() { openAboutWindow(); }
+            // },
+            // {	type: "separator" },
+            {	label: 'Start minimized',
+                type: 'checkbox',
+                checked: config.readSettings('startup:startMinimized'),
+                click: function(menuItem) {
+                    config.saveSettings('startup:startMinimized', menuItem.checked);
+                },
+            },
+            {	label: 'Start at login',
+                type: 'checkbox',
+                checked: config.readSettings('startup:startAtLogin'),
+                click: function(menuItem) {
+                    config.saveSettings('startup:startAtLogin', menuItem.checked);
+                    // test on Mac with:  osascript -e 'tell application "System Events" to get the name of every login item'
+                    if( menuItem.checked ) {
+                        blink1ControlAutoLauncher.enable();
+                    } else {
+                        blink1ControlAutoLauncher.disable();
+                    }
+                }
+            },
+            {	label: 'Enable API server',
+                type: 'checkbox',
+                checked: config.readSettings('apiServer:enabled'),
+                click: function(menuItem) {
+                    config.saveSettings('apiServer:enabled', menuItem.checked);
+                    // mainWindow.webContents.send('reloadConfig', 'apiServer');
+                }
+            },
+            {	type: "separator" },
+            { 	label: 'Set Status...',
+                submenu: statusButtons
+            },
+            {	type: "separator" },
+            {	label: 'Open Settings...',
+                click: function() {
+                    console.log("Open Settings...");
+                    mainWindow.show();
+                }
+            },
+            {	label: 'Off / Reset Alerts',  // Note: only works when DevTools is hiddden, else Cmd-r reloads
+                // accelerator: 'CommandOrControl+R',	// accelerator: 'CmdOrCtrl+R',
+                click: function() {
+                    // mainWindow.webContents.send('resetAlerts', 'woot');
+                    PatternsService.stopAllPatterns();
+                    Blink1Service.off();
+                }
+            },
+            {	label: 'Quit',
+                // accelerator: 'CommandOrControl+Q',
+                // selector: 'terminate:',
+                click: function() {
+                    // app.quit(); // doesn't actually cause app to quit
+            		ipcRenderer.send('quitnow');
+                }
+            }
+        ];
+        var contextMenu = Menu.buildFromTemplate( contextMenuTemplate );
+        tray.setContextMenu(contextMenu);
+        if (process.platform === 'darwin') {
+            app.dock.setMenu( contextMenu ); // Make Dock have same context menu
+        }
 
     },
 
     setupTrayMenu: function() {
+        var self = this;
         // if( process.env.NODE_ENV === 'development' ) {
         // contextMenuTemplate = contextMenuTemplate.concat( devMenuTemplate );
         // }
-        var contextMenu = Menu.buildFromTemplate( contextMenuTemplate );
-
         if( process.platform === 'win32' ) {  // FIXME: make this icon better for Windows
             // tray = new Tray( path.join(__dirname, './images/icons/blink1mk2-icon2-128px.ico') );
             tray = new Tray('./app/images/icons/blink1mk2-icon2-128px.ico' );
@@ -150,7 +156,8 @@ var trayMaker = {
             tray = new Tray('./app/images/icons/blink1mk2-icon-16px.png' );
         }
         tray.on('click', function() {
-            console.log("tray CLICK!");
+            console.log("TRAY MENU CLICK!");
+            self.updateTrayMenu();
             tray.popUpContextMenu();
         });
         tray.on('double-click', function() {
@@ -158,16 +165,14 @@ var trayMaker = {
             // mainWindow.show();
         });
         tray.setToolTip( pkg.productName + ' is running...');
-        tray.setContextMenu(contextMenu);
 
-        if (process.platform === 'darwin') {
-            app.dock.setMenu( contextMenu ); // Make Dock have same context menu
-        }
         // delete tray object to eliminate duplicates on reload
         window.onbeforeunload = function(/*e*/) {
             console.log("killing tray");
             if(tray) { tray.destroy(); tray = null; }
         };
+
+        // this.updateTrayMenu();
 
     }
 
