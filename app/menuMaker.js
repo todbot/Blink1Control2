68ff37832581d779a1668fa2a4d7eb98585eee79
Tod E. Kurt <tod@todbot.com>
2017-08-31 22:29:33 -0700

    clean up menus, start on platform-specific menus

--- app/menuMaker.js
+++ app/menuMaker.js
@@ -6,7 +6,6 @@ var Tray = remote.Tray;
 var app = remote.app;
 var ipcRenderer = require('electron').ipcRenderer;
 
-var pkg = require('./package.json');
 var config = require('./configuration');
 var log = require('./logger');
 var Eventer = require('./eventer');
@@ -15,6 +14,7 @@ var Blink1Service = require('./server/blink1Service');
 
 var tray = null;
 
+var myname = app.getName();
 
 var MenuMaker = {
 
@@ -58,7 +58,7 @@ var MenuMaker = {
         contextMenuTemplate = contextMenuTemplate.concat( bigButtonMenu );
 
         var contextMenuTemplateB = [
-            {  type: "separator" },
+            {  type: 'separator' },
             {  label: 'Off / Reset Alerts',
                 accelerator: resetShortcut,
                 click: function() {
@@ -70,13 +70,13 @@ var MenuMaker = {
             //         // FIXME: TBD
             //     }
             // },
-            {	type: "separator" },
+            {	type: 'separator' },
             {	label: 'Open Controls...',
                 click: function() {
                     ipcRenderer.send('openMainWindow');
                 }
             },
-            {	type: "separator" }
+            {	type: 'separator' }
         ];
         var contextMenuTemplateC = [
             {	label: 'Quit',
@@ -88,69 +88,6 @@ var MenuMaker = {
         contextMenuTemplate = contextMenuTemplate.concat( contextMenuTemplateB );
         // Array.prototype.push.apply( contextMenuTemplate, contextMenuTemplateB );
 
-            // {	label: 'About ' + pkg.productName,
-            // 	click: function() { openAboutWindow(); }
-            // },
-            // {	type: "separator" },
-            // {	label: 'Start minimized',
-            //     type: 'checkbox',
-            //     checked: config.readSettings('startup:startMinimized'),
-            //     click: function(menuItem) {
-            //         config.saveSettings('startup:startMinimized', menuItem.checked);
-            //     },
-            // },
-            // {	label: 'Start at login',
-            //     type: 'checkbox',
-            //     checked: config.readSettings('startup:startAtLogin'),
-            //     click: function(menuItem) {
-            //         config.saveSettings('startup:startAtLogin', menuItem.checked);
-            //         // test on Mac with:  osascript -e 'tell application "System Events" to get the name of every login item'
-            //         if( menuItem.checked ) {
-            //             blink1ControlAutoLauncher.enable();
-            //         } else {
-            //             blink1ControlAutoLauncher.disable();
-            //         }
-            //     }
-            // },
-            //
-            /*
-            {	label: 'Off / Reset Alerts',  // Note: only works when DevTools is hiddden, else Cmd-r reloads
-                // accelerator: 'CommandOrControl+R',	// accelerator: 'CmdOrCtrl+R',
-                click: function() {
-                    // mainWindow.webContents.send('resetAlerts', 'woot');
-                    PatternsService.stopAllPatterns();
-                    Blink1Service.off();
-                }
-            },
-            {	type: "separator" },
-            {	label: 'Enable API server',
-                type: 'checkbox',
-                checked: config.readSettings('apiServer:enabled'),
-                click: function(menuItem) {
-                    config.saveSettings('apiServer:enabled', menuItem.checked);
-                    // mainWindow.webContents.send('reloadConfig', 'apiServer');
-                }
-            },
-            // {	type: "separator" },
-            // { 	label: 'Set Status...',
-            //     submenu: statusButtons
-            // },
-            {	type: "separator" },
-            {	label: 'Open Settings...',
-                click: function() {
-                    console.log("Open Settings...");
-                    mainWindow.show();
-                }
-            },
-            {	label: 'Quit',
-                // accelerator: 'CommandOrControl+Q',
-                // selector: 'terminate:',
-                click: function() {
-                    // app.quit(); // doesn't actually cause app to quit
-            		ipcRenderer.send('quitnow');
-                }
-            }
-        ];*/
 
         var contextMenu = Menu.buildFromTemplate( contextMenuTemplate );
         if (process.platform === 'darwin') {
@@ -170,7 +107,7 @@ var MenuMaker = {
         else {
             tray = new Tray( app.getAppPath() + '/images/icons/blink1mk2-icon-16px.png' );
         }
-        tray.setToolTip( pkg.productName + ' is running...');
+        tray.setToolTip( myname + ' is running...');
 
         // delete tray object to eliminate duplicates on reload
         window.onbeforeunload = function(/*e*/) {
@@ -188,11 +125,8 @@ var MenuMaker = {
                 ipcRenderer.send('openMainWindow');
             });
         }
-        // tray.on('right-click', function() {
-        //     self.showTrayMenu();
-        // });
-        // tray.on('double-click', function() {
-        // });
+        // tray.on('right-click', function() { self.showTrayMenu(); });
+        // tray.on('double-click', function() { });
     },
 
     setupMainMenu: function() {
@@ -211,70 +145,107 @@ var MenuMaker = {
             { label: 'Off / Reset Alerts', accelerator: resetShortcut, click: function() {
                 mainWindow.webContents.send('resetAlerts');
             }},
-            { type: "separator" }
+            { type: 'separator' }
         ];
 
         controlMenuTemplate = controlMenuTemplate.concat( bigButtonMenu );
 
         // Mac-specific menu  (hide, unhide, etc. enables Command-Q )
-        var templateMac = [
-            {	label: pkg.productName,
+        var templateAppMac = [
+            {	label: myname,
                 submenu: [
-                    // { label: "About Blink1Control", selector: "orderFrontStandardAboutPanel:" },
-                    { label: "About Blink1Control2",
+                    { label: 'About ' + myname,
                         click: function() {
                             ipcRenderer.send('openAboutWindow');
                         }
                     },
-                    // FIXME: make appropriate OS-specific version of Windows
                     { type: 'separator' },
-                    { label: 'Hide Blink1Control2', accelerator: "CommandOrControl+H", role: 'hide' },
-                    { label: 'Hide Others', accelerator: 'CommandOrControl+Shift+H', role: 'hideothers' },
-                    { label: 'Show All', role: 'unhide' },
-                    { type: "separator" },
                     { label: 'Preferences...', accelerator: "CommandOrControl+,",
                         click: function() {
                             ipcRenderer.send('openPreferences');
                         }
                     },
-                    { type: "separator" },
+                    { type: 'separator' },
+                    { role: 'hide' },
+                    { role: 'hideothers' },
+                    { role: 'unhide' },
+                    { type: 'separator' },
                     { label: 'Open Controls...', accelerator: 'CommandOrControl+O',
                         click: function() {
                             ipcRenderer.send('openMainWindow');
                         }
                     },
-                    { type: "separator" },
-                    { label: 'Open DevTools', accelerator: 'Alt+CommandOrControl+I',
-                        click: function() {
-                            ipcRenderer.send('openDevTools');
-                        }
-                    },
-                    { type: "separator" },
-                    { label: 'Close Window', accelerator: "CommandOrControl+W", role:'close' },
-                    { type: "separator" },
+                    { type: 'separator' },
+                    { role: 'toggledevtools',label: 'Toggle Dev Tools' },
+                    { type: 'separator' },
+                    { role: 'close' },
+                    { type: 'separator' },
                     { label: "Quit", accelerator: "CommandOrControl+Q",
                         click: function() {
                              ipcRenderer.send('quitnow');
                         }
                     }
                 ]
-            },
+            }
+        ];
+
+        var templateApp = [
+            {	label: myname,
+                submenu: [
+                    { label: 'About ' + myname,
+                        click: function() {
+                            ipcRenderer.send('openAboutWindow');
+                        }
+                    },
+                    { type: 'separator' },
+                    { label: 'Preferences...', accelerator: "CommandOrControl+,",
+                        click: function() {
+                            ipcRenderer.send('openPreferences');
+                        }
+                    },
+                    { role: 'toggledevtools', label: 'Toggle Dev Tools' },
+                    { type: 'separator' },
+                    { role: 'close' },
+                    { type: 'separator' },
+                    { label: "Quit", accelerator: "CommandOrControl+Q",
+                        click: function() {
+                             ipcRenderer.send('quitnow');
+                        }
+                    }
+                ]
+            }
+        ];
+
+        var templateEdit = [
             {	label: "Edit",
                 submenu: [
                     // { label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo" },
                     // { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo" },
-                    // { type: "separator" },
-                    { label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut" },
-                    { label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy" },
-                    { label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste" },
-                    { label: "Select All", accelerator: "CmdOrCtrl+A", role: "selectall" }
+                    // { type: 'separator' },
+                    { role: "cut" },
+                    { role: "copy" },
+                    { role: "paste" },
+                    { role: "delete" },
+                    { role: "selectall" }
                 ]
             },
+        ];
+        var templateControl = [
             { label: "Control",
                 submenu: controlMenuTemplate
             }
         ];
-        Menu.setApplicationMenu(Menu.buildFromTemplate(templateMac));
+
+        var template = [];
+        if( process.platform === 'darwin' ) {
+            template = templateAppMac.concat(templateEdit,templateControl);
+        }
+        else {
+            template = templateApp.concat(templateEdit,templateControl);
+        }
+        console.log("template = ", template);
+
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
     }
 
