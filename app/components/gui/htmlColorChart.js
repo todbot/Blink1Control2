bc8b89975318118d535c1d70e66810210e024a61
Tod E. Kurt <tod@todbot.com>
2018-01-11 23:06:12 -0800

    ColorPicker: switch from table to PNG + SVG overlay, faster!

--- app/components/gui/htmlColorChart.js
+++ app/components/gui/htmlColorChart.js
@@ -7,45 +7,109 @@ var React = require('react');
 
 var htmlcolors = require('./htmlColorsList');
 
+// var colorChartPng = require('../../images/colorChart.png');
+var cw = 12; // cell width
+var ch = 13;  // cell height
+
+
 var HtmlColorChart = React.createClass({
     propTypes: {
         handleClick: React.PropTypes.func,
         currentColor: React.PropTypes.string
     },
-    handleColorClick: function(color) {
-        this.down = true;
+    getInitialState: function() {
+        return {
+            down:false, x:0,y:0, color:'#000000'
+        }
+    },
+    // shouldComponentUpdate: function(nextProps, nextState) {
+    //     if (this.props.color !== nextProps.color) {
+    //         return true;
+    //     }
+    //     return false;
+    // },
+    determineColor: function(x,y) {
+      var c = Math.floor(x/cw);
+      var r = Math.floor(y/ch);
+      var color = htmlcolors[r][c];
+      return color;
+    },
+    getXY: function(evt) {
+      var dim = evt.target.getBoundingClientRect();
+      var x = evt.clientX - dim.left;
+      var y = evt.clientY - dim.top;
+      return {x,y};
+    },
+    sendColor: function(evt) {
+        var {x,y} = this.getXY(evt);
+        var color = this.determineColor(x,y);
+        this.setState({x,y,color});
         this.props.handleClick(color);
     },
-    handleColorUp: function() {
-        this.down = false;
+    convertXYtoCR: function(x,y) {
+        var c = Math.floor(x/cw);
+        var r = Math.floor(y/ch);
+        return {c,r};
     },
-    handleColorMove: function(color) {
-        if( this.down ) {
-            this.props.handleClick(color);
+    getXYForColor: function(color) {
+        var x=-1, y=-1;
+        for( var i=0; i< htmlcolors.length; i++ ) {
+            for( var j=0; j< htmlcolors[0].length; j++ ) {
+                if( htmlcolors[i][j] === color ) {
+                    var x = (j*cw), y = (i*ch);
+                    // console.log("match at:",j,i, "=>", (j*cw),(i*ch), "::"); //, self.state.x, self.state.y);
+                    return {x,y};
+                }
+            }
+        }
+        return {x,y};
+    },
+
+    handleColorClick: function(evt) {
+        this.setState({down:true});
+        this.sendColor(evt);
+    },
+    handleColorUp: function() {
+        this.setState({down:false});
+    },
+    handleColorMove: function(evt) {
+        if( this.state.down ) {
+            this.sendColor(evt);
         }
     },
     render: function() {
-        var createCell = function(color,i) {
-            var w = (i===24) ? 15 : 12;
-            var borderCurr = (color.toUpperCase()===this.props.currentColor.toUpperCase())? '2px solid #bbb' : '';
-            return (
-                <td onMouseDown={this.handleColorClick.bind(null,color)}
-                    onMouseOver={this.handleColorMove.bind(null,color)}
-                    onMouseUp={this.handleColorUp}
-                    key={color}
-                    style={{width:w, height:13, background:color, outline:borderCurr }}></td>
-            );
-        };
-        var createRow = function(colorrow,i) {
-            return (<tr key={i}>{colorrow.map(createCell,this)}</tr>);
-
-        };
+        var self = this;
+        // find closest color box and make outline for it
+        var createBoxCoords = function(x,y) {
+            if( x === -1 && y===-1 ) {
+                return '';
+            }
+            x = Math.floor(x/cw) * cw + (cw/2);
+            y = Math.floor(y/ch) * ch + (ch/2);
+            var str = '';
+            str +=  (x-7)+','+(y-7)+' ' +  // top left
+                    (x+7)+','+(y-7)+' ' +  // top right
+                    (x+7)+','+(y+7)+' ' +  // bottom right
+                    (x-7)+','+(y+7)+' ' +
+                    (x-7)+','+(y-7);  // top left
+            return str;
+        }
+        var {x,y} = self.getXYForColor( this.props.currentColor.toUpperCase() );
+        // console.log("currColor:",this.props.currentColor, "xy:",x,y);
         return (
-            <table style={{borderCollapse:'collapse', outine:'1px solid #eee', cursor:'crosshair' }}>
-                <tbody>
-                {htmlcolors.map(createRow,this)}
-                </tbody>
-            </table>
+            <div>
+              <div style={{position:'relative', display:'inline-block' }} >
+                <img width={600/2} height={390/2} src="images/colorChart.png" alt=""
+                  onMouseDown={this.handleColorClick}
+                  onMouseMove={this.handleColorMove}
+                  onMouseUp  ={this.handleColorUp}
+                  style={{display:'block', maxWidth:'100%', height:'auto', WebkitUserDrag:'none'}}
+                />
+                <svg width={600/2} height={390/2} style={{position:'absolute',top:0, left:0, pointerEvents:'none'}}>
+                  <polyline stroke="#bbb" strokeWidth="4" fill="none" points={createBoxCoords(x,y)} />
+                </svg>
+              </div>
+            </div>
         );
     }
 });
