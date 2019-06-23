872fcdd3d5910490e4023b1f3659a1cba109452c
Tod E. Kurt <tod@todbot.com>
2019-06-23 14:30:30 -0700

    fix whitespace

--- app/logger.js
+++ app/logger.js
@@ -21,42 +21,43 @@ if( !logconfig.ignoredSources ) { logconfig.ignoredSources = []; }
 
 
 var Logger = {
-	/**
-	 * Log a message
-	 *
-	 * @method function
-	 * @return {[type]} [description]
-	 */
-	msg: function(/* msg,msg,msg */) {
-		var iargs = arguments;
-        if( logconfig.showDebug ) {
-			var ignore = logconfig.ignoredSources.some( function(is) {
-				return iargs[0].toString().match(is) ;
-			});
-			if( ignore ) { return; }
+  /**
+  * Log a message
+  *
+  * @method function
+  * @return {[type]} [description]
+  */
 
-			var args = Array.prototype.slice.call(arguments);
-			// args.unshift( Math.floor(new Date().getTime()/1000) + ':');
-			args.unshift( new Date().getTime() + ':');
-            console.log.apply(console, args );
-        }
-        else {
-            // do nothing, but later, log to file?
-        }
-		// FIXME: log to file?
-	},
-	warn: function() {
-		var args = Array.prototype.slice.call(arguments);
-		args.unshift( Math.floor(new Date().getTime()/1000) + ':');
-		console.warn.apply(console, args);
-		// FIXME: log to file?
-	},
-	error: function() {
-		var args = Array.prototype.slice.call(arguments);
-		args.unshift( Math.floor(new Date().getTime()/1000) + ':');
-		console.error.apply(console, args);
-		// FIXME: log to file?
-	}
+  msg: function(/* msg,msg,msg */) {
+    var iargs = arguments;
+    if( logconfig.showDebug ) {
+      var ignore = logconfig.ignoredSources.some( function(is) {
+        return iargs[0].toString().match(is) ;
+      });
+      if( ignore ) { return; }
+
+      var args = Array.prototype.slice.call(arguments);
+      // args.unshift( Math.floor(new Date().getTime()/1000) + ':');
+      args.unshift( new Date().getTime() + ':');
+      console.log.apply(console, args );
+    }
+    else {
+      // do nothing, but later, log to file?
+    }
+    // FIXME: log to file?
+  },
+  warn: function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift( Math.floor(new Date().getTime()/1000) + ':');
+    console.warn.apply(console, args);
+    // FIXME: log to file?
+  },
+  error: function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift( Math.floor(new Date().getTime()/1000) + ':');
+    console.error.apply(console, args);
+    // FIXME: log to file?
+  }
 };
 
 module.exports = Logger;
